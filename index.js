16c98d020db3cb30b7bca21bf363a749978fbfb6
Joan Nyawira Kori <korijoan171@gmail.com>
2025-03-04 22:43:35 +0300

    adds math operators

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1; // Generates a number between 1 and 100
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // 16 % 6 = 4
+
+const max = Math.max(1, 9, 17, 20); // Returns 20
